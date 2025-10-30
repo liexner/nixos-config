137e7f4eaf153b1958ce4ee198e5c1e7f810a8f4
Linus <linus.exnerademark@combitech.com>
2025-10-30 13:02:24 +0000

    removed some comments

--- modules/machines/nixos/_common/default.nix
+++ modules/machines/nixos/_common/default.nix
@@ -1,17 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Shared across all NixOS hosts
-  
-  # Common packages
+
   environment.systemPackages = with pkgs; [
     wget
     git
   ];
 
-  # Nix settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Default state version
   system.stateVersion = "25.05";
 }
