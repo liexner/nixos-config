650247b3772b5c2bd969fa2b00affc63b67e7780
Linus <liexner@gmail.com>
2026-09-04 07:48:54 +0200

    added baseline

--- flake.nix
+++ flake.nix
@@ -36,11 +36,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
   };
 
   outputs =
--- modules/programs/neovim/default.nix
+++ modules/programs/neovim/default.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   flake.modules.nixos.neovim =
-    { ... }:
+    { pkgs, ... }:
     {
       imports = [ inputs.nixvim.nixosModules.nixvim ];
 
@@ -10,6 +10,42 @@
         defaultEditor = true;
         viAlias = true;
         vimAlias = true;
+
+        colorschemes.tokyonight.enable = true;
+
+        globals.mapleader = " ";
+
+        extraPackages = [ pkgs.ripgrep ];
+
+        plugins.mini = {
+          enable = true;
+          modules = {
+            statusline = { };
+            files = { };
+            pick = { };
+          };
+        };
+
+        keymaps = [
+          {
+            mode = "n";
+            key = "<leader>e";
+            action = "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0))<CR>";
+            options.desc = "Open file explorer";
+          }
+          {
+            mode = "n";
+            key = "<leader>ff";
+            action = "<cmd>lua MiniPick.builtin.files()<CR>";
+            options.desc = "Find files";
+          }
+          {
+            mode = "n";
+            key = "<leader>fg";
+            action = "<cmd>lua MiniPick.builtin.grep_live()<CR>";
+            options.desc = "Grep in files";
+          }
+        ];
       };
     };
 }
