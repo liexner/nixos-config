55b17a28e49aa898e2fafa561fa7331fffcd9d64
Linus <liexner@gmail.com>
2026-09-04 09:19:24 +0200

    added yank for wsl

--- modules/programs/neovim/default.nix
+++ modules/programs/neovim/default.nix
@@ -11,11 +11,91 @@
         viAlias = true;
         vimAlias = true;
 
-        colorschemes.tokyonight.enable = true;
+        # colorschemes.tokyonight.enable = true;
 
         globals.mapleader = " ";
 
-        extraPackages = [ pkgs.ripgrep ];
+        opts = {
+          background = "dark"; # pin dark background instead of relying on terminal auto-detection
+          number = true; # line number
+          relativenumber = true; # relative line numbers
+          cursorline = true; # highlight current line
+          wrap = false; # do not wrap lines by default
+          scrolloff = 10; # keep 10 lines above/below cursor
+          sidescrolloff = 10; # keep 10 lines to left/right of cursor
+
+          tabstop = 2; # tabwidth
+          shiftwidth = 2; # indent width
+          softtabstop = 2; # soft tab stop not tabs on tab/backspace
+          expandtab = true; # use spaces instead of tabs
+          smartindent = true; # smart auto-indent
+          autoindent = true; # copy indent from current line
+
+          ignorecase = true; # case insensitive search
+          smartcase = true; # case sensitive if uppercase in string
+          hlsearch = true; # highlight search matches
+          incsearch = true; # show matches as you type
+
+          signcolumn = "yes"; # always show a sign column
+          showmatch = true; # highlights matching brackets
+          cmdheight = 1; # single line command line
+          completeopt = "menuone,noinsert,noselect"; # completion options
+          showmode = false; # do not show the mode, instead have it in statusline
+          pumheight = 10; # popup menu height
+          pumblend = 10; # popup menu transparency
+          winblend = 0; # floating window transparency
+          conceallevel = 0; # do not hide markup
+          concealcursor = ""; # do not hide cursorline in markup
+          synmaxcol = 300; # syntax highlighting limit
+          fillchars = {
+            eob = " ";
+          }; # hide "~" on empty lines
+
+          backup = false; # do not create a backup file
+          writebackup = false; # do not write to a backup file
+          swapfile = false; # do not create asswapfile
+          undofile = true; # do create an undo file
+          updatetime = 300; # faster completion
+          timeoutlen = 500; # timeout duration
+          ttimeoutlen = 10; # key code timeout (0 causes terminal query responses, e.g. background color, to fragment into literal keystrokes)
+          autoread = true; # auto-reload changes if outside of neovim
+          autowrite = false; # do not auto-save
+
+          hidden = true; # allow hidden buffers
+          errorbells = false; # no error sounds
+          backspace = "indent,eol,start"; # better backspace behaviour
+          autochdir = false; # do not autochange directories
+          selection = "inclusive"; # include last char in selection
+          # mouse = "a"; # enable mouse support
+          modifiable = true; # allow buffer modifications
+          encoding = "utf-8"; # set encoding
+        };
+
+        extraConfigLua =
+          # lua
+          ''
+            local undodir = vim.fn.expand("~/.vim/undodir")
+            if vim.fn.isdirectory(undodir) == 0 then
+              vim.fn.mkdir(undodir, "p")
+            end
+            vim.opt.undodir = undodir
+
+            vim.opt.iskeyword:append("-") -- include - in words
+            vim.opt.path:append("**") -- include subdirs in search
+          '';
+
+        extraPackages = [
+          pkgs.ripgrep
+        ];
+
+        highlightOverride = {
+          Normal.bg = "NONE";
+          NormalNC.bg = "NONE";
+          NormalFloat.bg = "NONE";
+          FloatBorder.bg = "NONE";
+          SignColumn.bg = "NONE";
+          EndOfBuffer.bg = "NONE";
+        };
 
         plugins.mini = {
           enable = true;
@@ -23,7 +103,14 @@
             statusline = { };
             files = { };
             pick = { };
+            comment = { };
           };
+        };
+
+        plugins.treesitter = {
+          enable = true;
+          highlight.enable = true;
+          indent.enable = true;
         };
 
         keymaps = [
@@ -46,6 +133,34 @@
             options.desc = "Grep in files";
           }
         ];
+
+        ########
+        # WSL
+        ########
+
+        clipboard.register = "unnamedplus";
+
+        extraConfigLuaPre =
+          # lua
+          ''
+            -- clip.exe / powershell.exe are on PATH by default via WSL interop, no
+            -- extra package needed. Paste is a bit slow since powershell.exe has to
+            -- spin up each time; switch to win32yank.exe later if that's annoying.
+            if vim.fn.has("wsl") == 1 then
+              vim.g.clipboard = {
+                name = "wsl-clipboard",
+                copy = {
+                  ["+"] = "clip.exe",
+                  ["*"] = "clip.exe",
+                },
+                paste = {
+                  ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                  ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+                },
+                cache_enabled = false,
+              }
+            end
+          '';
       };
     };
 }
