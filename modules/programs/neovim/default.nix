9d4bc119a90a7f3d1f6d6aaf3a3a634d40f6f580
Linus <liexner@gmail.com>
2026-09-04 09:31:12 +0200

    added lazygit as a floating window

--- modules/programs/neovim/default.nix
+++ modules/programs/neovim/default.nix
@@ -104,6 +104,7 @@
             files = { };
             pick = { };
             comment = { };
+            diff = { };
           };
         };
 
@@ -133,6 +134,42 @@
             options.desc = "Grep in files";
           }
         ];
+
+        ########
+        # Git
+        ########
+
+        extraConfigLuaPost =
+          # lua
+          ''
+            -- Open lazygit (push/pull/merge/branch/etc.) in a floating terminal.
+            -- Reuses the system lazygit install rather than a separate git plugin.
+            local function toggle_lazygit()
+              local buf = vim.api.nvim_create_buf(false, true)
+              local width = math.floor(vim.o.columns * 0.9)
+              local height = math.floor(vim.o.lines * 0.9)
+              local win = vim.api.nvim_open_win(buf, true, {
+                relative = "editor",
+                width = width,
+                height = height,
+                row = math.floor((vim.o.lines - height) / 2),
+                col = math.floor((vim.o.columns - width) / 2),
+                style = "minimal",
+                border = "rounded",
+              })
+              vim.fn.jobstart("lazygit", {
+                term = true,
+                on_exit = function()
+                  if vim.api.nvim_win_is_valid(win) then
+                    vim.api.nvim_win_close(win, true)
+                  end
+                end,
+              })
+              vim.cmd("startinsert")
+            end
+
+            vim.keymap.set("n", "<leader>gg", toggle_lazygit, { desc = "Open lazygit" })
+          '';
 
         ########
         # WSL
